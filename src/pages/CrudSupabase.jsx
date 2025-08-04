234bda4852b06f761fa764ee1dddac6331ffd60f
Jonathan Flores <jonathan.flores@mail.udp.cl>
2025-08-03 22:16:16 -0400

    feat: add sonner for toast notifications; implement form handling in CrudSupabase with validation for task input

--- src/pages/CrudSupabase.jsx
+++ src/pages/CrudSupabase.jsx
@@ -1,13 +1,37 @@
-import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery } from "@tanstack/react-query";
 import { useTareasStore } from "../store/TareasStore";
 import { Icon } from "@iconify/react/dist/iconify.js";
+import { useForm } from "react-hook-form";
+import { toast, Toaster } from "sonner";
 
 export const CrudSupabase = () => {
-  const { mostrarTareas } = useTareasStore();
+  const { mostrarTareas, insertarTareas } = useTareasStore();
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm();
   const { data, isLoading, error } = useQuery({
     queryKey: ["mostrar tareas"],
     queryFn: mostrarTareas,
   });
+  const {mutate,isPending} = useMutation({
+    mutationKey: ["insertar tarea"],
+    mutationFn: async (data) => {
+      const p = {
+        nombre: data.nombre,
+      };
+      await insertarTareas();
+    },
+    onError: (error) => {
+      toast.error("Error:" + error.message);
+    },
+    onSuccess: () => {
+      toast.success("Registro guardado correctamente");
+    },
+  });
+
   if (isLoading) {
     return <span>cargando...</span>;
   }
@@ -16,12 +40,21 @@ export const CrudSupabase = () => {
   }
   return (
     <main className="min-h-screen bg-amber-300 flex items-center justify-center p-4">
+      <Toaster position="top-right" richColors />
       <div className="bg-white p-6 rounded-xl shadow-xl w-full max-w-md">
         <h1 className="text-2xl font-bold text-center text-black mb-4">
           Tareas - SUPABASE + REACT
         </h1>
-        <form className="flex gap-2 mb-4">
-          <input className="flex-1 border p-2 rounded-md focus:outline-none focus:border-0 focus:ring-2 focus:ring-amber-400" />
+        <form onSubmit={handleSubmit()} className="flex gap-2 mb-4">
+          <input
+            {...register("nombre", { required: "La tarea es obligatoria" })}
+            type="text"
+            className="flex-1 border p-2 rounded-md focus:outline-none focus:border-0 focus:ring-2 focus:ring-amber-400"
+            placeholder="Escribe una tarea"
+          />
+          {errors.nombre && (
+            <p className="text-red-500 text-sm mb-2">{errors.nombre.message}</p>
+          )}
           <button className="bg-amber-400 text-black font-bold px-4 rounded hover:bg-amber-300 cursor-pointer">
             Agregar
           </button>
